31d1dabf6d6b36da51af637817aa54e22906ad57
Steve Stone <sstone@vectra.ai>
2024-04-03 18:03:26 +0100

    Tab to complete, CR is normal, Escape to exit cmp window

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,13 +1,33 @@
 return {
-  "nvim-cmp",
-  ---@param opts cmp.ConfigSchema
-  opts = function(_, opts)
-    local cmp = require("cmp")
-    opts.completion.completeopt = "menu,menuone,noinsert,noselect"
-    opts.mapping["<CR>"] = cmp.mapping.confirm({ select = false })
-    opts.window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-    }
-  end,
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+  {
+    "nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      f = require("cmp_nvim_lsp")
+      f = require("cmp_buffer")
+      opts.completion.completeopt = "menu,menuone,noinsert"
+      opts.mapping["<Tab>"] = cmp.mapping.confirm({ select = true })
+      opts.mapping["<CR>"] = function(fallback)
+        fallback()
+      end
+      opts.mapping["<Esc>"] = function(fallback)
+        if cmp.visible() then
+          cmp.close()
+        else
+          fallback()
+        end
+      end
+      opts.window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      }
+    end,
+  },
 }
