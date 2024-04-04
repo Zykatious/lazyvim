0bc39f09e3b674d51a8c97c8403552b330bf21a7
Steve Stone <sstone@vectra.ai>
2024-04-04 20:28:38 +0100

    Copilot things

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -10,12 +10,23 @@ return {
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local cmp = require("cmp")
-      opts.completion.completeopt = "menu,menuone,noinsert"
+      opts.completion.completeopt = "menu,menuone,noinsert,noselect"
       opts.mapping["<Tab>"] = cmp.mapping.confirm({ select = true })
       opts.mapping["<CR>"] = function(fallback)
-        fallback()
+        if cmp.get_active_entry() then
+          cmp.confirm({ select = false })
+        else
+          fallback()
+        end
       end
       opts.mapping["<Esc>"] = function(fallback)
+        if cmp.get_active_entry() then
+          cmp.close()
+        else
+          fallback()
+        end
+      end
+      opts.mapping["<C-Cr>"] = function(fallback)
         if cmp.visible() then
           cmp.close()
         else
