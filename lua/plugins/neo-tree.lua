5b93d52eac455a3b4d4821767acf5449fd6e3b4a
Steve Stone <sstone@vectra.ai>
2024-04-04 10:34:49 +0100

    Toggle Neotree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,23 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  cmd = "Neotree",
+  keys = {
+    {
+      "<leader>e",
+      function()
+        if vim.bo.filetype == "neo-tree" then
+          vim.cmd.wincmd("p")
+        else
+          vim.cmd.Neotree("focus")
+        end
+      end,
+      desc = "Toggle Explorer Focus",
+    },
+  },
+  opts = {
+    window = {
+      width = 30,
+    },
+  },
+}
